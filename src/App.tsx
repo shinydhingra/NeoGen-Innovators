abfac32c38ad0919699224a804efe570b9fde2d5
shinydhingra <shinydhingra2006@gmail.com>
2026-07-24 19:49:07 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Founders from './components/Founders';
+import Team from './components/Team';
+import Achievements from './components/Achievements';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      <About />
+      <Founders />
+      <Team />
+      <Achievements />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
